ec2d9b0ad90e74c5d3cb2f84196400ce14dc5e8e
Alex <luckyone@tutamail.com>
2022-05-06 23:38:44 -0400

    Finished recipe-list style

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -6,7 +6,7 @@ const IngredientList = ({ ingredients }) => {
     return <Ingredient key={ingredient.id} {...ingredient} />
   })
 
-  return <div>{ingredientElements}</div>
+  return <div className="ingredient-grid">{ingredientElements}</div>
 }
 
 export default IngredientList
